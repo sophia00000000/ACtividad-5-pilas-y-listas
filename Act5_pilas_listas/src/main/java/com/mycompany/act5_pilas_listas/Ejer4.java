4c9d221beb42f09ab6aad48a044b7ef96ec8416b
sophia <laura.hernandez10@usa.edu.co>
2024-09-30 15:56:19 -0500

    xd

--- Act5_pilas_listas/src/main/java/com/mycompany/act5_pilas_listas/Ejer4.java
+++ Act5_pilas_listas/src/main/java/com/mycompany/act5_pilas_listas/Ejer4.java
@@ -1,13 +1,42 @@
 package com.mycompany.act5_pilas_listas;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Stack;
 
 public class Ejer4 {
-	public static ArrayList<Stack<Stack<Integer>>> bodega = new ArrayList();
+	public static ArrayList<Stack<Stack<String>>> bodega = new ArrayList();
 	
 	public static void ejecutar(){
+		llenarBodega();
+		mostrarBodega();
+		sacarContenedor( ejm );
+	}
+	
+	public static void llenarBodega() {
+	// Arreglo principal de 9 pilas
+	int cantidadFilas=9; //9 pilas
+	int cantidadPilasPorFila=7; //7 pilas en cada una de las 9 filas
+	int cantidadPorPila=8; // alto de cada pila
+	
+		for (int i = 0; i < cantidadFilas; i++) {
+            Stack< Stack<String> > pilaFila = new Stack<>();
+            
+            for (int j = 0; j < cantidadPorPila; j++) {
+            	Stack<String> pila = new Stack<>();
+            	
+            	for (int k = 0; k < cantidadPorPila; j++) {
+            		Random random = new Random();
+      	
+            		pila.push((char) ('A' + random.nextInt(26)) + String.format("%04d", random.nextInt(10000)));
+            	}
+            	pilaFila.push(pila);
+            }          
+            bodega.add(pilaFila);
+		}
+
+	}
+	public static void sacarContenedor(String codigo) {
 		
 	}
-
 }
---
+++ Act5_pilas_listas/src/main/java/com/mycompany/act5_pilas_listas/Ejer5.java
@@ -0,0 +1,5 @@
+package com.mycompany.act5_pilas_listas;
+
+public class Ejer5 {
+
+}
